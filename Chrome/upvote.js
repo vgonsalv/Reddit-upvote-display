82d99cac913800fb5e69ee8988e4d74ba482dd92
victor Gonsalves <vgonsalv@pudue.edu>
2014-06-24 13:09:01 -0400

    supports db based vote getting and sending

--- Chrome/upvote.js
+++ Chrome/upvote.js
@@ -54,6 +54,13 @@ function Votes(up, down) {
     this.down = down;
 }
 
+function LinkTag(linkTag) {
+    this.tag = linkTag;
+    this.shortlink = (/t3_([a-z0-9]+)/).exec(linkTag.getAttribute("data-fullname"))[1];
+    this.entryTag = linkTag.getElementsByClassName("entry")[0];
+    this.taglineTag = this.entryTag.getElementsByClassName("tagline")[0];
+}
+
 
 function findInfoTag(element) {
     return new InfoTag(element.getElementsByClassName("linkinfo")[0]);
@@ -66,15 +73,46 @@ function findMidcolTag(element) {
         }
     }
 }
-function sendVotes(shortlink,infoTag){
-    var xmlHttp = null;
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("POST", "http://localhost:8080/Reddit-upvote-display/votecount", true);
-    xmlHttp.setRequestHeader("Content-type", "text/plain");
-    var obj = {shortlink:shortlink,score:infoTag.scoreTag.getScore(),likes:infoTag.scoreTag.getLikes()};
-    xmlHttp.send(JSON.stringify(obj));
+function findLinkTags() {
+    var table = document.getElementById("siteTable");
+    var linkTags = table.getElementsByClassName("thing link");
+    var links = [];
+    for (var i = 0; i < linkTags.length; i++) {
+        links.push(new LinkTag(linkTags[i]));
+    }
+    return links;
 }
 
+
+function sendVotes(shortlink, infoTag) {
+    var xmlHttp = null;
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST", "http://redditupvotedisplay-env.elasticbeanstalk.com/votecount", true);
+    xmlHttp.setRequestHeader("Content-type", "text/plain");
+    var obj = {action: "add",
+        posts: [
+            {shortlink: shortlink, score: infoTag.scoreTag.getScore(), likes: infoTag.scoreTag.getLikes()}
+        ]};
+    xmlHttp.send(JSON.stringify(obj));
+}
+function getVotes(request) {
+    var xmlHttp = null;
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("POST", "http://redditupvotedisplay-env.elasticbeanstalk.com/votecount", false);
+    xmlHttp.setRequestHeader("Content-type", "text/plain");
+    xmlHttp.send(JSON.stringify(request));
+    var data = JSON.parse(xmlHttp.responseText);
+    var voteData = [];
+    for (var i = 0; i < request.posts.length; i++) {
+        if (data[i].score !== null) {
+            var votes = calculateVotes(data[i].score, data[i].likes / 100);
+            voteData[i] = votes;
+        }else{
+            voteData[i] = new Votes("?","?");
+        }
+    }
+    return voteData;
+}
 function getPage(url) {
     var xmlHttp = null;
     xmlHttp = new XMLHttpRequest();
@@ -89,16 +127,14 @@ function formatVotes(midcolTag, votes) {
     midcolTag.setUnContent('+' + votes.up + ' -' + votes.down);
     midcolTag.setDisContent('+' + votes.up + ' -' + (votes.down + 1));
 
-    var width = Math.ceil(Math.log(Math.max(votes.up + 1, votes.down + 1)) / Math.LN10) + 1;
+    var width = Math.ceil(Math.log(Math.max(votes.up + 1, votes.down + 1)+.1) / Math.LN10) + 1;
     midcolTag.setWidth(width * .9 + 'em');
 }
 
-function calculateVotes(ScoreTag) {
+function calculateVotes(score, percent) {
     var votes = new Votes();
-    var percent = ScoreTag.getLikes() / 100;
-    var netVotes = ScoreTag.getScore();
-    votes.up = Math.round(percent * netVotes / (2 * percent - 1));
-    votes.down = Math.round(netVotes * (1 - percent) / (2 * percent - 1));
+    votes.up = Math.round(percent * score / (2 * percent - 1));
+    votes.down = Math.round(score * (1 - percent) / (2 * percent - 1));
     return votes;
 }
 function adjustVotes(midcolTag, votes) {
@@ -113,16 +149,32 @@ function adjustVotes(midcolTag, votes) {
             break;
     }
 }
+function inComments() {
+    return (/.*reddit.com\/r\/[^\/]+\/comments\/.*/).test(document.URL);
+}
 
-
-var midcolTag = findMidcolTag(document);
-var infoTag = findInfoTag(document.body);
-if (infoTag.scoreTag.getLikes() !== 50) {
-    var votes = calculateVotes(infoTag.scoreTag);
-    formatVotes(midcolTag, votes);
-    var unparsed_link = infoTag.shortlinkTag.linkBox.getAttribute("value");
-    var result = (/http:\/\/redd.it\/([a-z0-9]+)/).exec(unparsed_link);
-    var shortlink = result[1];
-    sendVotes(shortlink,infoTag);
+if (inComments()) {
+    var midcolTag = findMidcolTag(document);
+    var infoTag = findInfoTag(document.body);
+    if (infoTag.scoreTag.getLikes() !== 50) {
+        var votes = calculateVotes(infoTag.scoreTag.getScore(), infoTag.scoreTag.getLikes() / 100);
+        formatVotes(midcolTag, votes);
+        var unparsed_link = infoTag.shortlinkTag.linkBox.getAttribute("value");
+        var result = (/http:\/\/redd.it\/([a-z0-9]+)/).exec(unparsed_link);
+        var shortlink = result[1];
+        sendVotes(shortlink, infoTag);
+    }
+} else {
+    var links = findLinkTags();
+    var shortlinks = [];
+    for (i in links) {
+        shortlinks[i] = {shortlink: links[i].shortlink};
+    }
+    var request = {action: "fetch", posts: shortlinks};
+    var voteInfo = getVotes(request);
+    for(i in shortlinks){
+        links[i].tag.setAttribute("data-ups",voteInfo[i].up);
+        links[i].tag.setAttribute("data-downs",voteInfo[i].down);
+    }
 }
 
