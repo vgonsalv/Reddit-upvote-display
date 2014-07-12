5c139fdd6b6c155533d5b11bf98d61077543fae5
victor Gonsalves <vgonsalv@pudue.edu>
2014-07-12 01:46:21 -0400

    removed exta files

--- version 2/Chrome/background.js
+++
@@ -1,8 +0,0 @@
-chrome.runtime.onMessageExternal.addListener(foo);
-chrome.runtime.onConnectExternal.addListener(foo);
-function foo(e){
-    console.log("foo");
-    if(e){
-        console.log(e);
-    }
-}
