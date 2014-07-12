4c5c0df83dd5c76f638544e830b03ec2fd02890c
victor Gonsalves <vgonsalv@pudue.edu>
2014-07-12 01:45:46 -0400

    removed potential cross script vulnrabilities

--- version 2/Chrome/chrome-restore-options.js
+++ version 2/Chrome/chrome-restore-options.js
@@ -1,19 +1,24 @@
-window.addEventListener("load",foo);
 chrome.storage.sync.get(RUDOption, function(ro) {
     RUDOption = ro;
     doUpvotes();
 });
-$(window).on("neverEndingLoad",foo);
 function addOptions(){
-    var iconURL = chrome.extension.getURL("icon16.png");
-    var optionURL = chrome.extension.getURL("options.html");
     var head = document.getElementById("header-bottom-right");
-    head.appendChild(create('<span class = "serparator">|</span><span><a target="_blank" href="'
-            +optionURL+'"><img src="'+iconURL+'"></a></span>'));
-}
-function foo(e){
-    console.log("foo");
-    if(e){
-        console.log(e);
-    }
+    var s = document.createElement("span");
+    s.className = "serparator";
+    s.appendChild(document.createTextNode("|"));
+    
+    head.appendChild(s);
+    
+    s = document.createElement("span");
+    var a = document.createElement("a");
+    a.href = chrome.extension.getURL("options.html");
+    a.setAttribute("target","_blank");
+    
+    var i = document.createElement("img");
+    i.src = chrome.extension.getURL("icon16.png");
+    
+    a.appendChild(i);
+    s.appendChild(a);
+    head.appendChild(s);
 }
